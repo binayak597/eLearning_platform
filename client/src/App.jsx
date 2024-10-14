0cdc7bb76e89cc754c37eb5619ef64e384e604bc
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2024-10-15 01:15:27 +0530

    minor bug fix in App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/header/header";
+import Header from "./components/header/Header";
 import Home from "./pages/Home/Home";
 import "./App.css";
 import Register from "./pages/Auth/Register";
